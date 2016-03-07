8e427f303a4b5a43654c93f188dcb72d3e97560d
Kylie Judd <kyliejudd@gmail.com>
2016-03-07 21:43:46 +0000

    private information from scripts

---
+++ powershell/vmware/Host config/addsyslogserver.ps1
@@ -0,0 +1,13 @@
+﻿# setting the syslog server destination and port, along with opening firewall ports.
+
+$SysLogServer = "FQDN/IP"
+$location = "clustername/datacenter/folder"
+
+Get-VMHost -Location $location | Foreach {
+    Write-Host "Adding $SysLogServer as Syslog server for $($_.Name)"
+    $SetSyslog = Set-VMHostSysLogServer -SysLogServer $SysLogServer -SysLogServerPort 514 -VMHost $_
+    Write-Host "Restarting Syslog on $($_.Name)"
+    $Reload = (Get-ESXCLI -VMHost $_).System.Syslog.reload()
+    Write-Host "Setting firewall to allow Syslog out of $($_)"
+    $FW = $_ | Get-VMHostFirewallException | Where {$_.Name -eq 'syslog'} | Set-VMHostFirewallException -Enabled:$true
+}
---
+++ powershell/vmware/Host config/get-mgmtnetwork.ps1
@@ -0,0 +1,4 @@
+﻿Get-VMHost * | Get-VMHostNetwork | Select Hostname, `
+VMKernelGateway -ExpandProperty `
+VirtualNic | Where {$_.ManagementTrafficEnabled} | Select Hostname, `
+PortGroupName, IP, SubnetMask | sort IP | out-default
