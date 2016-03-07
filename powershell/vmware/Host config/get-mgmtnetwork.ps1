d3ce1a9008fe58de5e2756a56b423074f6724558
Kylie Judd <kyliejudd@gmail.com>
2016-03-07 21:51:34 +0000

    restructure folders

--- powershell/vmware/Host config/get-mgmtnetwork.ps1
+++
@@ -1,4 +0,0 @@
-﻿Get-VMHost * | Get-VMHostNetwork | Select Hostname, `
-VMKernelGateway -ExpandProperty `
-VirtualNic | Where {$_.ManagementTrafficEnabled} | Select Hostname, `
-PortGroupName, IP, SubnetMask | sort IP | out-default
